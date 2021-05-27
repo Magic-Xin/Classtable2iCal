a5e75ec21688c59a85779592b1a2093eef731de7
MagicXin <x.yu2000@163.com>
2021-05-27 16:23:29 +0800

    update frontend

--- Frontend/src/main.js
+++ Frontend/src/main.js
@@ -1,8 +1,12 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import MuseUI from 'muse-ui';
+import 'muse-ui/dist/muse-ui.css';
 
-Vue.config.productionTip = false
+Vue.use(MuseUI);
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+    el: '#app',
+    render (h) {
+        return h('mu-button', {}, 'Hello World');
+    }
+});
